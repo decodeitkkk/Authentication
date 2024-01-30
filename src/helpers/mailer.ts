b5ce511ebaada40bf05d88dae7c6d937d98bd834
decodeitkkk <kkailash0086@gmail.com>
2024-01-30 15:29:48 +0530

    setting up gmail

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,50 +1,65 @@
-import User from "@/models/userModel"
-import nodemailer from "nodemailer"
-import bcryptjs from "bcryptjs"
+import User from "@/models/userModel";
+import nodemailer from "nodemailer";
+import bcryptjs from "bcryptjs";
 
-export const sendEmail = async ({email,emailType,userId}:any) =>{
-    try{
-        const hashedToken = await bcryptjs.hash(userId.toString(),10)
+export const sendEmail = async ({ email, emailType, userId }: any) => {
+    try {
+        const hashedToken = await bcryptjs.hash(userId.toString(), 10);
 
         //  TO GET INDIAN DATE & TIME
-        let d = Date.now()
+        let d = Date.now();
         let options = {
-            timeZone:"Asia/Kolkata",
-            hour12:false
-        }
+            timeZone: "Asia/Kolkata",
+            hour12: false,
+        };
 
-        if(emailType === "VERIFY"){
-            await User.findByIdAndUpdate(userId,{
-                verifyToken:hashedToken, verifyTokenExpiry: Date.now() + 3600000
-            })
-        } else if( emailType === "RESET"){
-            await User.findByIdAndUpdate(userId,{
-                forgotPasswordToken:hashedToken, forgotPasswordTokenExpiry: Date.now() + 3600000
-            })
+        if (emailType === "VERIFY") {
+            await User.findByIdAndUpdate(userId, {
+                verifyToken: hashedToken,
+                verifyTokenExpiry: Date.now() + 3600000,
+            });
+        } else if (emailType === "RESET") {
+            await User.findByIdAndUpdate(userId, {
+                forgotPasswordToken: hashedToken,
+                forgotPasswordTokenExpiry: Date.now() + 3600000,
+            });
         }
 
         let transport = nodemailer.createTransport({
-            host: "sandbox.smtp.mailtrap.io",
-            port: 2525,
+            service: process.env.SERVICE,
+            // host: "sandbox.smtp.mailtrap.io",
+            // port: 2525,
+            // service:"gmail",
             auth: {
-              user: process.env.NODEMAILER_USER,
-              pass: process.env.NODEMAILER_PASS
-            }
+                user: process.env.GMAIL_USER,
+                pass: process.env.GMAIL_PASS,
+            },
         });
 
         const mailOptions = {
-            from:"decodeitkkk@gmail.com",
-            to:email,
-            subject:emailType === "VERIFY" ? "Verify Your Email" : "Reset Your Password",
+            from: "kailashdaphnis@gmail.com",
+            to: email,
+            subject:
+                emailType === "VERIFY"
+                    ? "Verify Your Email"
+                    : "Reset Your Password",
             // html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">Here</a> to ${emailType === "VERIFY" ? "Verify Your Email" : "Reset Your Password"}. or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken} </p>` ,
             html: `<p>Click 
-            <a href=" ${emailType === "VERIFY" ? process.env.DOMAIN+"/verifyemail?token="+hashedToken : process.env.DOMAIN+"/resetpassword?token="+hashedToken } ">Here</a> to ${emailType === "VERIFY" ? "Verify Your Email" : "Reset Your Password"}. or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken} </p>` ,
-        }
+            <a href=" ${
+                emailType === "VERIFY"
+                    ? process.env.DOMAIN + "/verifyemail?token=" + hashedToken
+                    : process.env.DOMAIN + "/resetpassword?token=" + hashedToken
+            } ">Here</a> to ${
+                emailType === "VERIFY"
+                    ? "Verify Your Email"
+                    : "Reset Your Password"
+            }. or copy and paste the link below in your browser. <br> ${
+                process.env.DOMAIN
+            }/verifyemail?token=${hashedToken} </p>`,
+        };
 
-        const mailResponse = await transport.sendMail(mailOptions)
-
- 
-    }catch(error:any) {
-        throw new Error(error.message)
+        const mailResponse = await transport.sendMail(mailOptions);
+    } catch (error: any) {
+        throw new Error(error.message);
     }
-}
+};
