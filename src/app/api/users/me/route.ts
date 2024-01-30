c2b360ed623b8e8d8e8d894d2ccf3cca400f4f4b
decodeitkkk <kkailash0086@gmail.com>
2024-01-30 13:40:20 +0530

    fixed return statement in me/route.ts

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -3,17 +3,16 @@ import { getDataFromToken } from "@/helpers/getDataFromToken";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 
-connect()
-export async function GET(request:NextRequest){
+connect();
+export async function GET(request: NextRequest) {
     try {
-        const userId  = await getDataFromToken(request)
-        let user = await User.findOne({_id:userId}).select("-password")
+        const userId = await getDataFromToken(request);
+        let user = await User.findOne({ _id: userId }).select("-password");
         return NextResponse.json({
-            message:`User found`,
-            data: user
-        })
-    } catch (error:any) {
-        NextResponse.json({message:error.message},{status:400})
-        
+            message: `User found`,
+            data: user,
+        });
+    } catch (error: any) {
+        return NextResponse.json({ message: error.message }, { status: 400 });
     }
 }
